94dc83713eaabbfc534d45baf2977149148e0fb0
Alexander Khotskov <khotskov.aa@phystech.edu>
2022-12-06 02:35:15 +0300

    Updated readme and implemented UI program for LR.

---
+++ lr.cpp
@@ -0,0 +1,26 @@
+#include <fstream>
+#include <iostream>
+
+#include "builder_from_bnf.hpp"
+#include "lr_parser.hpp"
+
+int main(int argc, char** argv) {
+  if (argc < 2) {
+    std::cerr << "Usage: " << argv[0] << " <path/to/grammar_in_bnf>\n";
+    return 1;
+  }
+  std::ifstream file(argv[1], std::ios::in);
+  if (!file.is_open()) {
+    std::cerr << "Error: No such file '" << argv[1] << "'\n";
+    return 1;
+  }
+
+  BuilderFromBNF builder(file);
+  LRParser parser(builder.build());
+
+  std::string str;
+  while (!std::cin.eof()) {
+    std::getline(std::cin, str, '\n');
+    std::cout << (parser.recognize(str) ? "Yes\n" : "No\n");
+  }
+}
